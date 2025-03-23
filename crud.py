780fe71cef3dd904e9bffceb98986bec85400ea8
PROTOTYPEJESUS <jesusandrade427@gmail.com>
2025-03-23 16:41:43 -0500

    Se agrega validacion para prevenir registrar usuarios duplicados, ademas de agregar el metodo strip() para evitar problemas con espacios en blanco asi como tambien informar al usuario que esta dejando un campo vacio

--- crud.py
+++ crud.py
@@ -2,3 +2,37 @@
 
 users = {} # Diccionario para almacenar usuarios
 
+# FUNCION PARA REGISTRAR USUARIOS
+def register_user():
+    print("\n--- BIENVENIDO AL SISTEMA DE REGISTRO DE NOTAS DE IMPULCES ---")
+    
+    while True:
+        email = input("Ingrese correo electrónico: ").strip()
+        if not email:
+            print("El correo electrónico no puede estar vacío.")
+            continue
+        if email in users:
+            print("Este correo ya está registrado. Intente con otro.\n")
+            return
+        break
+
+    while True:
+        name = input("Ingrese su primer nombre: ").strip()
+        if name:
+            break
+        print("El nombre no puede estar vacío.")
+
+    while True:
+        lastname = input("Ingrese su primer apellido: ").strip()
+        if lastname:
+            break
+        print("El apellido no puede estar vacío.")
+
+    while True:
+        password = input("Ingrese contraseña: ").strip()
+        if password:
+            break
+        print("La contraseña no puede estar vacía.")
+
+    users[email] = {"name": name, "lastname": lastname, "password": password, "grades": {}}
+    print("Usuario registrado con éxito.\n")
