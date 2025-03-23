33f628795b5dadd786a9388942829e4097498bd2
PROTOTYPEJESUS <jesusandrade427@gmail.com>
2025-03-23 17:03:13 -0500

    Se crea la función login, la cual incluye un contador hasta 3 de intentos fallidos y que autentica en el diccionario users el email y la contraseña

--- crud.py
+++ crud.py
@@ -36,3 +36,24 @@ def register_user():
 
     users[email] = {"name": name, "lastname": lastname, "password": password, "grades": {}}
     print("Usuario registrado con éxito.\n")
+
+# FUNCION PARA INICIAR SESION
+def login():
+    print("\n--- INICIAR SESIÓN EN IMPULCES NOTAS ---")
+    
+    for _ in range(3):  # Permitir hasta 3 intentos de verificacion
+        email = input("Ingrese correo electrónico: ").strip()
+        password = input("Ingrese contraseña: ").strip()
+
+        if email in users and users[email]["password"] == password:
+            print(f"Bienvenido, {users[email]['name']} {users[email]['lastname']}.\n")
+            return True  # Usuario autenticado y devuelve nombre completo
+
+        print("Usuario o contraseña incorrectos. Intente nuevamente.\n")
+
+    print("Demasiados intentos fallidos. Intente más tarde.\n")
+    return False  # Inicio de sesión fallido
+
+# PRUEBA
+register_user()
+login()
